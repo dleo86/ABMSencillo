667008ac97d59da2f7de41cd0f2c3f51265c56bc
leandro <dleo86@gmail.com>
2019-08-23 18:38:00 -0300

    Utilizo el IDE Visual Studio 2017

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace BaseDeDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnAlta = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombreBaja = new System.Windows.Forms.TextBox();
+            this.btnBaja = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreActual = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNuevoNombre = new System.Windows.Forms.TextBox();
+            this.txtNuevaClave = new System.Windows.Forms.TextBox();
+            this.btnModifica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(74, 60);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(168, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Clave:";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(74, 107);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(168, 20);
+            this.txtClave.TabIndex = 3;
+            // 
+            // btnAlta
+            // 
+            this.btnAlta.Location = new System.Drawing.Point(142, 148);
+            this.btnAlta.Name = "btnAlta";
+            this.btnAlta.Size = new System.Drawing.Size(100, 30);
+            this.btnAlta.TabIndex = 4;
+            this.btnAlta.Text = "Alta";
+            this.btnAlta.UseVisualStyleBackColor = true;
+            this.btnAlta.Click += new System.EventHandler(this.btnAlta_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre:";
+            // 
+            // txtNombreBaja
+            // 
+            this.txtNombreBaja.Location = new System.Drawing.Point(74, 222);
+            this.txtNombreBaja.Name = "txtNombreBaja";
+            this.txtNombreBaja.Size = new System.Drawing.Size(168, 20);
+            this.txtNombreBaja.TabIndex = 6;
+            // 
+            // btnBaja
+            // 
+            this.btnBaja.Location = new System.Drawing.Point(142, 266);
+            this.btnBaja.Name = "btnBaja";
+            this.btnBaja.Size = new System.Drawing.Size(100, 30);
+            this.btnBaja.TabIndex = 7;
+            this.btnBaja.Text = "Baja";
+            this.btnBaja.UseVisualStyleBackColor = true;
+            this.btnBaja.Click += new System.EventHandler(this.btnBaja_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nombre a Modificar:";
+            // 
+            // txtNombreActual
+            // 
+            this.txtNombreActual.Location = new System.Drawing.Point(107, 338);
+            this.txtNombreActual.Name = "txtNombreActual";
+            this.txtNombreActual.Size = new System.Drawing.Size(135, 20);
+            this.txtNombreActual.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nuevo Nombre:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 454);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Nueva Clave:";
+            // 
+            // txtNuevoNombre
+            // 
+            this.txtNuevoNombre.Location = new System.Drawing.Point(107, 407);
+            this.txtNuevoNombre.Name = "txtNuevoNombre";
+            this.txtNuevoNombre.Size = new System.Drawing.Size(135, 20);
+            this.txtNuevoNombre.TabIndex = 12;
+            // 
+            // txtNuevaClave
+            // 
+            this.txtNuevaClave.Location = new System.Drawing.Point(107, 454);
+            this.txtNuevaClave.Name = "txtNuevaClave";
+            this.txtNuevaClave.Size = new System.Drawing.Size(135, 20);
+            this.txtNuevaClave.TabIndex = 13;
+            // 
+            // btnModifica
+            // 
+            this.btnModifica.Location = new System.Drawing.Point(142, 505);
+            this.btnModifica.Name = "btnModifica";
+            this.btnModifica.Size = new System.Drawing.Size(100, 30);
+            this.btnModifica.TabIndex = 14;
+            this.btnModifica.Text = "Modificar";
+            this.btnModifica.UseVisualStyleBackColor = true;
+            this.btnModifica.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 561);
+            this.Controls.Add(this.btnModifica);
+            this.Controls.Add(this.txtNuevaClave);
+            this.Controls.Add(this.txtNuevoNombre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNombreActual);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBaja);
+            this.Controls.Add(this.txtNombreBaja);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAlta);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnAlta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombreBaja;
+        private System.Windows.Forms.Button btnBaja;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombreActual;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNuevoNombre;
+        private System.Windows.Forms.TextBox txtNuevaClave;
+        private System.Windows.Forms.Button btnModifica;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BaseDeDatos
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection conexion;
+
+        private SqlDataAdapter adaptador;
+
+        private DataSet datos;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexion = new SqlConnection("Data Source=DESKTOP-N6FU7L1\\SQLEXPRESS;Initial Catalog=tp2;Integrated Security=True"); //AQUÍ VA EL STRING DE CONEXIÓN DE SU PC 
+            adaptador = new SqlDataAdapter();
+
+            SqlCommand alta = new SqlCommand("insert into usuarios (nombre, clave) values (@nombre, @clave)", conexion); 
+            
+             adaptador.InsertCommand = alta;
+            adaptador.InsertCommand.Parameters.Add(new SqlParameter("@nombre", SqlDbType.VarChar));
+            adaptador.InsertCommand.Parameters.Add(new SqlParameter("@clave", SqlDbType.VarChar));
+
+            SqlCommand baja = new SqlCommand("delete from usuarios where nombre=@nombre", conexion);
+            adaptador.DeleteCommand = baja;
+            adaptador.DeleteCommand.Parameters.Add(new SqlParameter("@nombre", SqlDbType.VarChar));
+
+            SqlCommand modificacion = new SqlCommand("update usuarios set nombre=@nombre, clave = @clave where nombre = @nombreant", conexion); 
+            
+            adaptador.UpdateCommand = modificacion;
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@nombre", SqlDbType.VarChar));
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@clave", SqlDbType.VarChar));
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@nombreant",SqlDbType.VarChar));
+            SqlCommand consulta = new SqlCommand("select nombre,clave from usuarios", conexion);
+            adaptador.SelectCommand = consulta;
+            datos = new DataSet();
+            conexion.Open();
+            adaptador.Fill(datos, "usuarios");
+            conexion.Close();
+          //  grillaUsuarios.DataSource = datos;
+           // grillaUsuarios.DataMember = "usuarios";
+        }
+
+        private void ActualizarDatos()
+        {
+            datos.Clear();
+            adaptador.Fill(datos, "usuarios");
+        }
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            adaptador.InsertCommand.Parameters["@nombre"].Value = txtNombre.Text;
+            adaptador.InsertCommand.Parameters["@clave"].Value = txtClave.Text;
+
+            try
+            {
+                conexion.Open();
+                adaptador.InsertCommand.ExecuteNonQuery();
+                ActualizarDatos();
+            }
+
+            catch (SqlException excepcion)
+            {
+
+                MessageBox.Show(excepcion.ToString());
+            }
+
+            finally
+            {
+                conexion.Close();
+            }
+        }
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+            adaptador.DeleteCommand.Parameters["@nombre"].Value = txtNombreBaja.Text;
+
+            try
+            {
+                conexion.Open();
+
+                int cantidad = adaptador.DeleteCommand.ExecuteNonQuery();
+
+                if (cantidad == 0)
+                {
+
+                    MessageBox.Show("No existe");
+                }
+                ActualizarDatos();
+            }
+
+            catch (SqlException excepcion)
+            {
+
+                MessageBox.Show(excepcion.ToString());
+            }
+
+            finally
+            {
+                conexion.Close();
+            }
+        }
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            adaptador.UpdateCommand.Parameters["@nombreant"].Value = txtNombreActual.Text;
+            adaptador.UpdateCommand.Parameters["@nombre"].Value = txtNuevoNombre.Text;
+            adaptador.UpdateCommand.Parameters["@clave"].Value = txtNuevaClave.Text;
+
+            try
+            {
+                conexion.Open();
+                adaptador.UpdateCommand.ExecuteNonQuery();
+                ActualizarDatos();
+            }
+
+            catch (SqlException excepcion)
+            {
+
+                MessageBox.Show(excepcion.ToString());
+            }
+
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
+
+
+
